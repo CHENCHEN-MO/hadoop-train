f3e77452e34812ad3fda010ea4c70c0d1251e8c9
CHENCHEN-MO <1937626297@qq.com>
2018-01-08 17:44:01 +0800

    hadoop lesson1

---
+++ src/test/java/com/chenchen/hadoop/hdfs/HDFSapp.java
@@ -0,0 +1,11 @@
+package com.chenchen.hadoop.hdfs;
+
+/**
+ * Created by Administrator on 2018/1/7.
+ */
+public class HDFSapp {
+
+    public void setUp() throws Exception{
+        System.out.println("HDFSapp.Set");
+    }
+}
